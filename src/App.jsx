9727a063a65b21bfcb5cf1c5f102f16cea7aad45
unknown <jacobvestergaard@hotmail.dk>
2025-03-12 15:27:05 +0100

    Card with count up when clicking

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 
 function Card({title}) {
-  const [hasLiked,setHasLiked] = useState(false)
+  const [count, setCount] = useState(0);
+  const [hasLiked,setHasLiked] = useState(false);
+
+  useEffect(() =>{
+    console.log(`${title} has been liked: ${hasLiked}`)
+  }, [hasLiked]);
+
 
   return(
-    <div className="card">
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount((prevState) => 
+    prevState + 1)}>
+      <h2>{title} <br/> {count || null}</h2>
 
       <button onClick={() => setHasLiked(!hasLiked)}>
        {hasLiked ? 'Liked' : 'Like'}
