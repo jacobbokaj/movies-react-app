03f8604cf3ea6d7c06e65234991a319ae8879300
unknown <jacobvestergaard@hotmail.dk>
2025-03-13 16:43:29 +0100

    MovieCard is added to show movies in more details

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Search from './components/search';
 import Spinner from './components/Spinner';
+import MovieCard from './components/MovieCard';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -76,7 +77,7 @@ function App() {
                 ): (
                     <ul>
                         {movieList.map((movie) => (
-                            <p key={movie.id} className="text-white">{movie.title}</p>
+                            <MovieCard key={movie.id} movie={movie}/>
                         ))}
                     </ul>
                 )}
