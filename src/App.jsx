32417ba36d2dbfe7152f32099203e89902dc8735
unknown <jacobvestergaard@hotmail.dk>
2025-03-13 12:16:58 +0100

    Added hero banner and text

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 
 function App() {
     return (
-        <div>App
-              <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <main>
+        <div className="pattern"/>
+            
+        <div className="wrapper">
+            <header>
+                <img src="./hero.png" alt="Hero Banner"/>
+                <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+            </header>
+              <p>Search</p>
         </div>
+     </main>
     )
 }
 
